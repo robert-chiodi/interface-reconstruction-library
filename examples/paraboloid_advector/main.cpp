34192e001457a9fa3bb45f2d052f9eaca6412754
Robert Chiodi <chiodi@illinois.edu>
2022-04-20 15:05:50 -0700

    Beginning work on a full paraboloid advector example.

---
+++ examples/paraboloid_advector/main.cpp
@@ -0,0 +1,72 @@
+#include <chrono>
+#include <iostream>
+#include <string>
+
+#include "examples/paraboloid_advector/circle_rotation_2d.h"
+#include "examples/paraboloid_advector/deformation_2d.h"
+#include "examples/paraboloid_advector/reconstruction_types.h"
+#include "examples/paraboloid_advector/solver.h"
+#include "examples/paraboloid_advector/vof_advection.h"
+
+static int startSimulation(const std::string& a_simulation_type,
+                           const std::string& a_advection_method,
+                           const std::string& a_reconstruction_method,
+                           const double a_time_step_size,
+                           const double a_time_duration,
+                           const int a_viz_frequency);
+
+int main(int argc, char* argv[]) {
+  if (argc != 7) {
+    std::cout << "Incorrect amount of command line arguments supplied. \n";
+    std::cout << "Arguments should be \n";
+    std::cout
+        << "Simulation to run. Options: Deformation2D, CircleRotation2D\n";
+    std::cout << "Advection method. Options: SemiLagrangian, "
+                 "SemiLagrangianCorrected, FullLagrangian\n";
+    std::cout << "Reconstruction method. Options: ELVIRA2D, LVIRA2D, MOF2D, "
+                 "AdvectedNormals, R2P2D\n";
+    std::cout << "Time step size, dt (double)\n";
+    std::cout << "Simulation duration(double)\n";
+    std::cout
+        << "Amount of time steps between visualization output (integer)\n";
+    std::exit(-1);
+  }
+
+  std::string simulation_type = argv[1];
+  std::string advection_method = argv[2];
+  std::string reconstruction_method = argv[3];
+  double time_step_size = std::stod(argv[4]);
+  double time_duration = std::stod(argv[5]);
+  int viz_frequency = atoi(argv[6]);
+
+  auto start = std::chrono::system_clock::now();
+  startSimulation(simulation_type, advection_method, reconstruction_method,
+                  time_step_size, time_duration, viz_frequency);
+  auto end = std::chrono::system_clock::now();
+  std::chrono::duration<double> runtime = end - start;
+  printf("Total run time: %20f \n\n", runtime.count());
+
+  return 0;
+}
+
+static int startSimulation(const std::string& a_simulation_type,
+                           const std::string& a_advection_method,
+                           const std::string& a_reconstruction_method,
+                           const double a_time_step_size,
+                           const double a_time_duration,
+                           const int a_viz_frequency) {
+  if (a_simulation_type == "CircleRotation2D") {
+    return runSimulation<CircleRotation2D>(
+        a_advection_method, a_reconstruction_method, a_time_step_size,
+        a_time_duration, a_viz_frequency);
+  } else if (a_simulation_type == "Deformation2D") {
+    return runSimulation<Deformation2D>(
+        a_advection_method, a_reconstruction_method, a_time_step_size,
+        a_time_duration, a_viz_frequency);
+  } else {
+    std::cout << "Unknown simulation type of : " << a_simulation_type << '\n';
+    std::cout << "Value entries are: CircleRotation2D, Deformation2D. \n";
+    std::exit(-1);
+  }
+  return -1;
+}
---
+++ examples/paraboloid_advector/reconstruction_types.h
@@ -0,0 +1,53 @@
+// This file is part of the Interface Reconstruction Library (IRL),
+// a library for interface reconstruction and computational geometry operations.
+//
+// Copyright (C) 2022 Robert Chiodi <robert.chiodi@gmail.com>
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+#ifndef EXAMPLES_PARABOLOID_ADVECTOR_RECONSTRUCTION_TYPES_H_
+#define EXAMPLES_PARABOLOID_ADVECTOR_RECONSTRUCTION_TYPES_H_
+
+#include <string>
+
+#include "irl/paraboloid_reconstruction/paraboloid.h"
+#include "irl/planar_reconstruction/planar_separator.h"
+
+#include "examples/paraboloid_advector/data.h"
+
+void getReconstruction(
+    const std::string& a_reconstruction_method,
+    const Data<double>& a_liquid_volume_fraction,
+    const Data<IRL::Pt>& a_liquid_centroid, const Data<IRL::Pt>& a_gas_centroid,
+    const Data<IRL::LocalizedParaboloidLink>& a_localized_paraboloid_link,
+    const double a_dt, const Data<double>& a_U, const Data<double>& a_V,
+    const Data<double>& a_W, Data<IRL::Paraboloid>* a_interface);
+
+struct KnownCircle {
+  void getReconstruction(const Data<double>& a_liquid_volume_fraction,
+                         const double a_dt, const Data<double>& a_U,
+                         const Data<double>& a_V, const Data<double>& a_W,
+                         Data<IRL::Paraboloid>* a_interface);
+};
+
+void correctInterfacePlaneBorders(Data<IRL::Paraboloid>* a_interface);
+
+namespace details {
+inline IRL::Paraboloid fromCircle(const IRL::Pt& a_center,
+                                  const double a_radius,
+                                  const IRL::Normal& a_normal) {
+  const double curvature = 1.0 / a_radius;
+  IRL::ReferenceFrame frame;
+  IRL::UnitQuaternion quat(M_PI * 0.5, IRL::Normal(0.0, 0.0, 1.0));
+  frame[0] = quat * a_normal;
+  frame[1] = IRL::Normal(0.0, 0.0, 1.0);
+  frame[2] = a_normal;
+
+  return IRL::Paraboloid(a_center + a_radius * a_normal, frame, 0.5 * curvature,
+                         0.0);
+}
+}  // namespace details
+
+#endif  // EXAMPLES_PARABOLOID_ADVECTOR_RECONSTRUCTION_TYPES_H_
